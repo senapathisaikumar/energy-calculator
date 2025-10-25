c0bc662f405da9a6d45f923fee971609f0a8f85e
SAIKUMAR90146 <saisenapathi325@gmail.com>
2025-10-25 16:12:39 +0530

    changed in api.js url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 // src/api.js
-// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api";
-const API_BASE_URL = "http://localhost:3000/api";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api";
+//const API_BASE_URL = "http://localhost:3000/api";
 
 async function apiFetch(path, options = {}) {
   const token = sessionStorage.getItem("token");
